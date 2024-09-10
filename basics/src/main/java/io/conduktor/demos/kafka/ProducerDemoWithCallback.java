b42273e4983663f04e44576754abec385ebba892
rookid19 <randyodoom19@gmail.com>
2024-09-10 19:20:32 +0000

    prducer with callback

---
+++ basics/src/main/java/io/conduktor/demos/kafka/ProducerDemoWithCallback.java
@@ -0,0 +1,64 @@
+package io.conduktor.demos.kafka;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemoWithCallback {
+
+    private static final Logger log  = LoggerFactory.getLogger(ProducerDemoWithCallback.class.getSimpleName());
+    public static void main(String[] args) {
+        log.info("Kafka with callback");
+
+        // create Producer Properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+
+        // create the Producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        for(int i = 0; i <10; i++){
+
+        // create a producer record
+        ProducerRecord<String, String>  producerRecord = new ProducerRecord<>("demo_java","hello world");
+
+        // send the data - asynchronous
+        producer.send(producerRecord, new Callback() {
+            @Override
+            public void onCompletion(RecordMetadata metadata, Exception exception) {
+                if(exception == null){
+                    // the record was successfully sent
+                    log.info("Received new metadata \n" +
+                            "Topic: " + metadata.topic() + "\n" +
+                            "Partition: " + metadata.partition() + "\n" +
+                            "Offset: " + metadata.offset() + "\n" +
+                            "Timestamp: " + metadata.timestamp());
+                }else{
+                    log.error("Error while producing ", exception);
+                }
+            }
+        });
+
+        try{
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        }
+
+
+        // flush means it should block all this line until all the data in producer is fully send
+        producer.flush();
+
+        // flush and close producer
+        producer.close();
+
+
+    }
+}
